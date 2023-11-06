18667d596195a2c7ff567f5560a7dd6565d54a93
Azy Sir <azysir@Azys-MacBook-Pro.local>
2023-11-06 23:11:24 +0000

    Updated the alias for init

--- cli/init.go
+++ cli/init.go
@@ -12,26 +12,32 @@ func Init() {
 	// Get Current Directory
 	currentDir, err := os.Executable()
 	shell := os.Getenv("SHELL")
+	log.Println("Getting Profile...")
 	profile := utils.GetShellProfile(shell)
 	if err != nil {
 		log.Fatal(("Error"))
 	}
-
+	log.Println("Writing Alias...")
 	writeAlias("azsume()", currentDir, profile)
 }
 
 func writeAlias(alias string, currentDir string, profile string) {
 	exist := checkAliasExists(profile, alias)
-	if !exist {
+	log.Printf("Exist: %s", exist)
+	if exist {
 		shellAlias := fmt.Sprintf(`
-%s {
+function %s {
+	clicommands=("init" "list" "get" "sync" "login")
 	if [ "$1" = "set" ]; then
-		eval "$(%s "$@")"
+		eval $("%[2]s" "$@")
+	elif [[ ! ("${clicommands[@]}" =~ "${1}") ]]; then
+		eval $("%[2]s" "$@")
 	else
 		%[2]s "$@"
 	fi
 }
 `, alias, currentDir)
+		log.Printf(shellAlias)
 		f, _ := os.OpenFile(profile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 		_, err := f.WriteString(shellAlias)
 		if err != nil {

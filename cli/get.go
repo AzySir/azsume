89f99fd771af9ccf4cdd57ca78ebbe82104554a1
Azy Sir <azysir@Azys-MacBook-Pro.local>
2023-11-06 22:47:45 +0000

    Fixed validation for length after set to less than 2

--- cli/get.go
+++ cli/get.go
@@ -8,7 +8,7 @@ import (
 
 // Get Credentials from .azure/credentials
 func Get(args []string) string {
-	if len(args) < 3 {
+	if len(args) < 2 {
 		log.Fatal("[ERROR] Please provide a name of the Credentials to Get")
 		os.Exit(1)
 	}

db9b9dcaf27a4ec753c42ba29a13d0b981c64e28
Azy Sir <azysir@Azys-MacBook-Pro.local>
2023-11-06 22:51:26 +0000

    Logic and error handling for section - this is to handle azsume <profile>

--- credentials/get.go
+++ credentials/get.go
@@ -4,18 +4,41 @@ import (
 	"azsume/utils"
 	"fmt"
 	"log"
+
+	"github.com/TwiN/go-color"
+	"gopkg.in/ini.v1"
 )
 
 func GetCredentials(args []string) string {
-	section := utils.GetSection(args[2])
-	result := ""
+	var result string
+	section, profile := getSection(args)
 	if len(section.KeysHash()) == 0 {
-		log.Fatal("[ERROR] This section does not exist")
+		if profile == "set" {
+			log.Fatal(color.Red + "[ERROR] Please select an Azure profile located in ~/.azure/credentials")
+		} else {
+			log.Fatalf(color.Red+"[ERROR] %s profile does not exist", profile)
+		}
 	}
 	for key, val := range section.KeysHash() {
 		result += fmt.Sprintf("export %s=%s\n", key, val)
 	}
-	result += fmt.Sprintf("export ARM_PROFILE=%s\n", args[2])
+	result += fmt.Sprintf("export ARM_PROFILE=%s\n", profile)
 
 	return result
+}
+
+func getSection(args []string) (*ini.Section, string) {
+	var section *ini.Section
+	var profile string
+	switch len(args) {
+	case 3:
+		section = utils.GetSection(args[2])
+		profile = args[2]
+	case 2:
+		section = utils.GetSection(args[1])
+		profile = args[1]
+	default:
+		log.Fatal(color.Red + "[ERROR] This section does not exist")
+	}
+	return section, profile
 }
